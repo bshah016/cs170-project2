39306231f0bae689d10c21ece53deef7bb06679a
Brij Shah <56499450+bshah016@users.noreply.github.com>
2022-11-21 15:02:42 -0800

    Update CS170FeatureSelection.py

--- CS170FeatureSelection.py
+++ CS170FeatureSelection.py
@@ -62,5 +62,36 @@ def forward(filename, num_features):
     #to avoid spurious precision: https://www.w3schools.com/python/ref_func_round.asp
     print('Finished search!! The best feature subset is ' + str(curr[max(curr.keys())]) +
           ' which has an accuracy of ' + str(round(max(curr.keys()), 3)) + '\n')
+    
+def leave_one_out_cross_validation(data, currset, feature_to_add):
+    number_correctly_classfied = 0
+    df = data.copy(deep=True)
+    # https://stackoverflow.com/questions/13187778/convert-pandas-dataframe-to-numpy-array
+    datadf = df.to_numpy(dtype='float', na_value=np.nan)
+    # Replace irrelevant features with 0
+    for i in range(1, feature_to_add):
+        if i not in currset:
+            datadf[:, i] = 0
+    for i in range(int(len(data.index))):
+        object_to_classify = datadf[i][1:]
+        label_object_to_classify = datadf[i][0]
+       #https://stackoverflow.com/questions/7781260/how-can-i-represent-an-infinite-number-in-python
+        nearest_n_dist = math.inf
+        nearest_n_loc = math.inf
+        for k in range(int(len(data.index))):
+            if k != i:
+                #Used:
+                #https://www.w3schools.com/python/ref_math_sqrt.asp#:~:text=The%20math.,than%20or%20equal%20to%200.
+                #https://www.geeksforgeeks.org/sum-function-python/#:~:text=Python%20provides%20an%20inbuilt%20function,of%20numbers%20in%20the%20iterable.
+                #distance = sqrt(sum((object_to_classify-data(k,2:end)).^2)); (from slides)
+                distance = math.sqrt(sum((object_to_classify - datadf[k][1:]) ** 2))
+                if distance < nearest_n_dist:
+                    nearest_n_dist = distance
+                    nearest_n_loc = k
+                    nearest_n_label = datadf[nearest_n_loc][0]
+        if label_object_to_classify == nearest_n_label:
+            number_correctly_classfied += 1
+    accuracy = number_correctly_classfied / int(len(data.index))
+    return accuracy
 
 main()
